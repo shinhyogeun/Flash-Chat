319bb5b970ca86b34393d52951655b07ef9c6c21
shinhyogeun <tls1gy2rms3@naver.com>
2020-04-17 19:10:37 +0900

    [Today's commit is about how to use firestore]

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,12 +15,7 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextfield: UITextField!
     let db = Firestore.firestore()
     var ref : DocumentReference? = nil
-    
-    var message : [Message] = [
-        Message(sender: "tls1gy2rms@naver.com", body: "우리가 위대해지는 순간"),
-        Message(sender: "tlsgyrms1231@naver.com", body: "그건 내가 결정한다."),
-        Message(sender: "tls1gy2rms@naver.com", body: "모두 사랑한다.")
-    ]
+    var message : [Message] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
@@ -33,15 +28,36 @@ class ChatViewController: UIViewController {
     
     func loadMessage(){
         message = []
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
-            if let e = error{
-                print("There are some problem retrieving from Firestore. \(e)")
-            } else {
-                querySnapshot
+        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+            if self.message.count == 0 {
+                if let e = error{
+                    print("There are some problem retrieving from Firestore. \(e)")
+                } else {
+                    if let snapShotDocument = querySnapshot?.documents{
+                        for doc in snapShotDocument {
+                            let data = doc.data()
+                            if let sender = data[K.FStore.senderField] as? String,
+                                let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: sender, body: messageBody)
+                                self.message.append(newMessage)
+                                DispatchQueue.main.async {self.tableView.reloadData()}
+                            }
+                        }
+                    }
+                }
+            }else{
+                querySnapshot?.documentChanges.forEach({ (diff) in
+                    if let sender = diff.document.data()[K.FStore.senderField] as? String,
+                        let messageBody = diff.document.data()[K.FStore.bodyField] as? String{
+                        let newMessage = Message(sender: sender, body: messageBody)
+                        self.message.append(newMessage)
+                        DispatchQueue.main.async {self.tableView.reloadData()}
+                    }
+                })
             }
-            <#code#>
         }
     }
+    
     
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -57,32 +73,33 @@ class ChatViewController: UIViewController {
                 }
             }
         }
-        ref = db.collection("tls1gy2rms3@naver.com").addDocument(data: [
-            "body" : messageTextfield.text ?? ""
-        ]) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(self.ref!.documentID)")
-            }
-        }
-
-        db.collection("tls1gy2rms3@naver.com").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    if self.ref!.documentID == document.documentID{
-                        var sender = "tls1gy2rms3@naver.com"
-                        var contents = document.data()["body"]!
-                        var madedMessage = Message(sender: sender, body: contents as! String)
-                        self.message.append(madedMessage)
-                        self.tableView.reloadData()
-                    }
-                }
-            }
-        }
-        messageTextfield.text = ""
+        //MARK: - 내가 만든 부분
+        //        ref = db.collection("tls1gy2rms3@naver.com").addDocument(data: [
+        //            "body" : messageTextfield.text ?? ""
+        //        ]) { err in
+        //            if let err = err {
+        //                print("Error adding document: \(err)")
+        //            } else {
+        //                print("Document added with ID: \(self.ref!.documentID)")
+        //            }
+        //        }
+        //
+        //        db.collection("tls1gy2rms3@naver.com").getDocuments() { (querySnapshot, err) in
+        //            if let err = err {
+        //                print("Error getting documents: \(err)")
+        //            } else {
+        //                for document in querySnapshot!.documents {
+        //                    if self.ref!.documentID == document.documentID{
+        //                        var sender = "tls1gy2rms3@naver.com"
+        //                        var contents = document.data()["body"]!
+        //                        var madedMessage = Message(sender: sender, body: contents as! String)
+        //                        self.message.append(madedMessage)
+        //                        self.tableView.reloadData()
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        messageTextfield.text = ""
     }
     
     @IBAction func LogOutPressed(_ sender: UIBarButtonItem) {
@@ -99,7 +116,7 @@ extension ChatViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return message.count
     }
-        //메시지창을 만드는 방법을 꼭 복습하자!!(쌈이랑 고기로 생각해보기!)
+    //메시지창을 만드는 방법을 꼭 복습하자!!(쌈이랑 고기로 생각해보기!)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as!MessageCell
         cell.label.text =  message[indexPath.row].body
