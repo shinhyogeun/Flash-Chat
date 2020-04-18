4812247c4ad0754aaa4cd74a33a7075038316ce7
shinhyogeun <tls1gy2rms3@naver.com>
2020-04-18 18:28:30 +0900

    [this update include IQkeyboardManagerSwift and little change in MessageCell]

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -15,7 +15,7 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var messageTextfield: UITextField!
     let db = Firestore.firestore()
     var ref : DocumentReference? = nil
-    var message : [Message] = []
+    var messages : [Message] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
@@ -27,44 +27,39 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessage(){
-        message = []
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
-            if self.message.count == 0 {
-                if let e = error{
-                    print("There are some problem retrieving from Firestore. \(e)")
-                } else {
-                    if let snapShotDocument = querySnapshot?.documents{
-                        for doc in snapShotDocument {
-                            let data = doc.data()
-                            if let sender = data[K.FStore.senderField] as? String,
-                                let messageBody = data[K.FStore.bodyField] as? String {
-                                let newMessage = Message(sender: sender, body: messageBody)
-                                self.message.append(newMessage)
-                                DispatchQueue.main.async {self.tableView.reloadData()}
+        db.collection(K.FStore.collectionName)
+          .order(by: K.FStore.dateField)
+          .addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            if let e = error{
+                print("There are some problem retrieving from Firestore. \(e)")
+            } else {
+                if let snapShotDocument = querySnapshot?.documents {
+                    for doc in snapShotDocument {
+                        let data = doc.data()
+                        if let sender = data[K.FStore.senderField] as? String,
+                            let messageBody = data[K.FStore.bodyField] as? String{
+                            let newMessage = Message(sender: sender, body: messageBody)
+                            self.messages.append(newMessage)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.messages.count-1, section: 0)
+                                self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true )
                             }
                         }
                     }
                 }
-            }else{
-                querySnapshot?.documentChanges.forEach({ (diff) in
-                    if let sender = diff.document.data()[K.FStore.senderField] as? String,
-                        let messageBody = diff.document.data()[K.FStore.bodyField] as? String{
-                        let newMessage = Message(sender: sender, body: messageBody)
-                        self.message.append(newMessage)
-                        DispatchQueue.main.async {self.tableView.reloadData()}
-                    }
-                })
             }
         }
     }
     
-    
-    
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email{
+            self.messageTextfield.text = ""
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField : messageSender,
-                K.FStore.bodyField : messageBody
+                K.FStore.bodyField : messageBody,
+                K.FStore.dateField : Date().timeIntervalSince1970
             ]) { (error) in
                 if let e = error {
                     print("There was an issue saving data in FireStore \(e)")
@@ -114,14 +109,28 @@ class ChatViewController: UIViewController {
 
 extension ChatViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return message.count
+        return messages.count
     }
     //메시지창을 만드는 방법을 꼭 복습하자!!(쌈이랑 고기로 생각해보기!)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as!MessageCell
-        cell.label.text =  message[indexPath.row].body
+        cell.label.text = message.body
+//         This is a message from current user.
+        if message.sender == Auth.auth().currentUser?.email {
+             cell.leftImageView.isHidden = true
+             cell.rightImageView.isHidden = false
+             cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+             cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }else{
+//         This is a message from current user.
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
         return cell
     }
-    
 }
 
